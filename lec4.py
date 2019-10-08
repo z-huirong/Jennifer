52912c9a4b63c87a1ebc8223c1c9a3d8b03af4f7
z-huirong <56325876+z-huirong@users.noreply.github.com>
2019-10-08 19:43:02 -0400

    Add files via upload

---
+++ lec4.py
@@ -0,0 +1,13 @@
+import sys
+print(sys.argv)
+v1=sys.argv[1]
+v2=sys.argv[2]
+file_name = 'first_10.csv'
+f = open(file_name)
+lines = f.readlines()
+for line in lines[1:]:
+    columns = line.split(',')
+    price = int(columns[9])
+    if price >= int(v1) and price <= int(v2):
+        print(line)
+
